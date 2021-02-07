98c4c409670adc982ae3457cf74c0a51fcd6a5fa
ruslanshkolnykh <32788135+ruslanshkolnykh@users.noreply.github.com>
2021-02-06 22:33:05 -0800

    Маршрут по тайным тропам (#4)
    * module2-task1
    
    * обновил линк
    
    Co-authored-by: Ruslan <ruslan.shkolnykh@protonmail.com>

--- src/components/not-found/not-found.jsx
+++ src/components/not-found/not-found.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import PlaceItem from "../place-item/place-item";
-import PropTypes from 'prop-types';
 import {Link} from "react-router-dom";
 
-const MainPage = (props) => {
-  const {itemAmount} = props;
+const NotFound = () => {
 
   return (<div className="page page--gray page--main">
     <header className="header">
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
-            <Link className="header__logo-link header__logo-link--active">
+            <Link className="header__logo-link header__logo-link--active" to="/">
               <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
             </Link>
           </div>
@@ -69,40 +66,17 @@ const MainPage = (props) => {
         </section>
       </div>
       <div className="cities">
-        <div className="cities__places-container container">
-          <section className="cities__places places">
-            <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">312 places to stay in Amsterdam</b>
-            <form className="places__sorting" action="#" method="get">
-              <span className="places__sorting-caption">Sort by</span>
-              <span className="places__sorting-type" tabIndex="0">
-                    Popular
-                <svg className="places__sorting-arrow" width="7" height="4">
-                  <use xlinkHref="#icon-arrow-select"></use>
-                </svg>
-              </span>
-              <ul className="places__options places__options--custom places__options--opened">
-                <li className="places__option places__option--active" tabIndex="0">Popular</li>
-                <li className="places__option" tabIndex="0">Price: low to high</li>
-                <li className="places__option" tabIndex="0">Price: high to low</li>
-                <li className="places__option" tabIndex="0">Top rated first</li>
-              </ul>
-            </form>
-            <div className="cities__places-list places__list tabs__content">
-              {Array(itemAmount).fill(itemAmount).map(() => <PlaceItem key={Math.random()} />)}
+        <div className="cities__places-container cities__places-container--empty container">
+          <section className="cities__no-places">
+            <div className="cities__status-wrapper tabs__content">
+              <b className="cities__status">404 Not Found</b>
             </div>
           </section>
-          <div className="cities__right-section">
-            <section className="cities__map map"></section>
-          </div>
+          <div className="cities__right-section"></div>
         </div>
       </div>
     </main>
   </div>);
 };
 
-MainPage.propTypes = {
-  itemAmount: PropTypes.number.isRequired,
-};
-
-export default MainPage;
+export default NotFound;
