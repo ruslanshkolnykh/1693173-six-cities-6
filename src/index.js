baad8b63308e5da04989207f72f1a2d7c5fe9e33
Ruslan <ruslan.shkolnykh@protonmail.com>
2021-03-13 11:05:14 -0800

    создан store в index а также обернул App в Provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import offers from './mocks/offers';
 import reviews from './mocks/reviews';
+import {createStore} from 'redux';
+import {reducer} from './redux/reducer';
+import {Provider} from 'react-redux';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-    <App
-      itemAmount={10}
-      offers={offers}
-      reviews={reviews}
-    />,
+    <Provider store ={store}>
+      <App
+        itemAmount={10}
+        offers={offers}
+        reviews={reviews}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -23,3 +23,5 @@ const reducer = (state = initialState, action) => {
   }
   return state;
 };
+
+export {reducer};
