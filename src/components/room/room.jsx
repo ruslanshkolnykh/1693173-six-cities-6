ac373481b8c4c9bbfbdb4f906aa83d324e4ba54d
Ruslan <ruslan.shkolnykh@protonmail.com>
2021-03-18 23:49:41 -0700

    добавил картинки в room

--- src/components/room/room.jsx
+++ src/components/room/room.jsx
@@ -35,22 +35,22 @@ const Room = (props) => {
         <div className="property__gallery-container container">
           <div className="property__gallery">
             <div className="property__image-wrapper">
-              <img className="property__image" src="img/room.jpg" alt="Photo studio" />
+              <img className="property__image" src={offer.images[0]} alt="Photo studio" />
             </div>
             <div className="property__image-wrapper">
-              <img className="property__image" src="img/apartment-01.jpg" alt="Photo studio" />
+              <img className="property__image" src={offer.images[1]} alt="Photo studio" />
             </div>
             <div className="property__image-wrapper">
-              <img className="property__image" src="img/apartment-02.jpg" alt="Photo studio" />
+              <img className="property__image" src={offer.images[2]} alt="Photo studio" />
             </div>
             <div className="property__image-wrapper">
-              <img className="property__image" src="img/apartment-03.jpg" alt="Photo studio" />
+              <img className="property__image" src={offer.images[3]} alt="Photo studio" />
             </div>
             <div className="property__image-wrapper">
-              <img className="property__image" src="img/studio-01.jpg" alt="Photo studio" />
+              <img className="property__image" src={offer.images[4]} alt="Photo studio" />
             </div>
             <div className="property__image-wrapper">
-              <img className="property__image" src="img/apartment-01.jpg" alt="Photo studio" />
+              <img className="property__image" src={offer.images[5]} alt="Photo studio" />
             </div>
           </div>
         </div>
